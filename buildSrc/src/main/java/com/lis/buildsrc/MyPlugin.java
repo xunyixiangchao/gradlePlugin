3c8df1b1bcf381ae3424f0e958f8916093a2b89b
lis <xunyixiangchao@qq.com>
2020-07-23 20:27:04 +0800

    firts commit

---
+++ buildSrc/src/main/java/com/lis/buildsrc/MyPlugin.java
@@ -0,0 +1,15 @@
+package com.lis.buildsrc;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
+
+public class MyPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        Logger logger = project.getLogger();
+        logger.error("=====================");
+        logger.error("最简单的Gradle插件");
+        logger.error("=====================");
+    }
+}
